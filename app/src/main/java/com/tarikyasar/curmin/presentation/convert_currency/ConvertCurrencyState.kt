30984b349b22a4809de0f43c2ddc6dd1d32a2807
Tarik Yasar <m.tarikyasar@gmail.com>
2022-10-06 20:56:25 +0300

    Remove unnecessary composables

--- app/src/main/java/com/tarikyasar/curmin/presentation/convert_currency/ConvertCurrencyState.kt
+++
@@ -1,9 +0,0 @@
-package com.tarikyasar.curmin.presentation.convert_currency
-
-import com.tarikyasar.curmin.domain.model.ConvertedCurrency
-
-data class ConvertCurrencyState(
-    val isLoading: Boolean = false,
-    val convertedCurrency: ConvertedCurrency? = null,
-    val error: String = ""
-)
--- app/src/main/java/com/tarikyasar/curmin/presentation/currency_symbol_list/CurrencySymbolListDropDownState.kt
+++
@@ -1,9 +0,0 @@
-package com.tarikyasar.curmin.presentation.currency_symbol_list
-
-import com.tarikyasar.curmin.domain.model.Symbol
-
-data class CurrencySymbolListDropDownState(
-    val isLoading: Boolean = false,
-    val currencySymbols: List<Symbol> = emptyList(),
-    val error: String = ""
-)
