9a947953c550a77750f7ed58a488fac4b36d6a3d
Tarik Yasar <m.tarikyasar@gmail.com>
2022-11-08 21:05:56 +0300

    Refactor currency detail screen

--- app/src/main/java/com/tarikyasar/curmin/presentation/screens/currency_detail/composable/CurrencyConversionSection.kt
+++ app/src/main/java/com/tarikyasar/curmin/presentation/screens/currency_detail/composable/CurrencyConversionSection.kt
@@ -1,24 +1,18 @@
 package com.tarikyasar.curmin.presentation.screens.currency_detail.composable
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tarikyasar.curmin.R
-import com.tarikyasar.curmin.presentation.ui.theme.CurrencyTextColor
 import com.tarikyasar.curmin.utils.CurrencyUtils
-import com.tarikyasar.curmin.utils.insert
 
 @Composable
 fun CurrencyConversionSection(
@@ -44,7 +38,6 @@ fun CurrencyConversionSection(
             )
         )
     }
-    var isBaseEditing by remember { mutableStateOf(true) }
 
     Column {
         Text(
@@ -56,122 +49,27 @@ fun CurrencyConversionSection(
         Spacer(Modifier.height(10.dp))
 
         Column {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                OutlinedTextField(
-                    value = baseCurrencyText,
-                    onValueChange = {
-                        if (isBaseEditing) {
-                            var valueText = it.text.filter { char ->
-                                char.isDigit()
-                            }
-
-                            val value = when {
-                                valueText.isEmpty() -> 0.0
-                                else -> {
-                                    valueText = valueText.insert(valueText.length - 2, '.')
-                                    valueText.toDouble()
-                                }
-                            }
-
-                            baseCurrencyText = TextFieldValue(
-                                text = CurrencyUtils.formatCurrency(value),
-                                selection = TextRange(CurrencyUtils.formatCurrency(value).length)
-                            )
-                            targetCurrencyText =
-                                TextFieldValue(text = CurrencyUtils.formatCurrency(value * currencyRate))
-
-                        }
-                    },
-                    modifier = Modifier
-                        .background(MaterialTheme.colors.surface)
-                        .fillMaxWidth()
-                        .onFocusEvent {
-                            if (it.isFocused) {
-                                isBaseEditing = true
-                            }
-                        }
-
-                )
-
-                Row(
-                    modifier = Modifier.align(Alignment.CenterEnd),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Divider(
-                        modifier = Modifier
-                            .height(40.dp)
-                            .width(1.dp)
-                            .background(CurrencyTextColor)
-                    )
-
-                    Text(
-                        text = baseCurrency,
-                        color = CurrencyTextColor,
-                        modifier = Modifier.padding(horizontal = 10.dp)
-                    )
+            CurrencyConversionTextField(
+                currencyName = baseCurrency,
+                currencyText = baseCurrencyText,
+                currencyRate = currencyRate,
+                onConversionCompleted = { base, target ->
+                    baseCurrencyText = base
+                    targetCurrencyText = target
                 }
-            }
+            )
 
             Spacer(Modifier.height(10.dp))
 
-            Box(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                OutlinedTextField(
-                    value = targetCurrencyText,
-                    onValueChange = {
-                        if (!isBaseEditing) {
-                            var valueText = it.text.filter { char ->
-                                char.isDigit()
-                            }
-
-                            val value = when {
-                                valueText.isEmpty() -> 0.0
-                                else -> {
-                                    valueText = valueText.insert(valueText.length - 2, '.')
-                                    valueText.toDouble()
-                                }
-                            }
-
-                            targetCurrencyText = TextFieldValue(
-                                text = CurrencyUtils.formatCurrency(value),
-                                selection = TextRange(CurrencyUtils.formatCurrency(value).length)
-                            )
-                            baseCurrencyText =
-                                TextFieldValue(text = CurrencyUtils.formatCurrency(value / currencyRate))
-                        }
-                    },
-                    modifier = Modifier
-                        .background(MaterialTheme.colors.surface)
-                        .fillMaxWidth()
-                        .onFocusEvent {
-                            if (it.isFocused) {
-                                isBaseEditing = false
-                            }
-                        }
-                )
-
-                Row(
-                    modifier = Modifier.align(Alignment.CenterEnd),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Divider(
-                        modifier = Modifier
-                            .height(40.dp)
-                            .width(1.dp)
-                            .background(CurrencyTextColor)
-                    )
-
-                    Text(
-                        text = targetCurrency,
-                        color = CurrencyTextColor,
-                        modifier = Modifier.padding(horizontal = 10.dp)
-                    )
+            CurrencyConversionTextField(
+                currencyName = targetCurrency,
+                currencyText = targetCurrencyText,
+                currencyRate = 1 / currencyRate,
+                onConversionCompleted = { base, target ->
+                    targetCurrencyText = base
+                    baseCurrencyText = target
                 }
-            }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/tarikyasar/curmin/presentation/screens/currency_detail/composable/CurrencyConversionTextField.kt
@@ -0,0 +1,89 @@
+package com.tarikyasar.curmin.presentation.screens.currency_detail.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.tarikyasar.curmin.presentation.ui.theme.CurrencyTextColor
+import com.tarikyasar.curmin.utils.CurrencyUtils
+import com.tarikyasar.curmin.utils.insert
+
+@Composable
+fun CurrencyConversionTextField(
+    currencyName: String,
+    currencyText: TextFieldValue,
+    currencyRate: Double,
+    onConversionCompleted: (TextFieldValue, TextFieldValue) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            value = currencyText,
+            onValueChange = {
+                val (base, target) = updateCurrencyAmount(
+                    amount = it,
+                    currencyRate = currencyRate
+                )
+
+                onConversionCompleted(base, target)
+            },
+            modifier = Modifier
+                .background(MaterialTheme.colors.surface)
+                .fillMaxWidth()
+        )
+
+        Row(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Divider(
+                modifier = Modifier
+                    .height(40.dp)
+                    .width(1.dp)
+                    .background(CurrencyTextColor)
+            )
+
+            Text(
+                text = currencyName,
+                color = CurrencyTextColor,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            )
+        }
+    }
+}
+
+private fun updateCurrencyAmount(
+    amount: TextFieldValue,
+    currencyRate: Double,
+): Pair<TextFieldValue, TextFieldValue> {
+    var valueText = amount.text.filter { char ->
+        char.isDigit()
+    }
+
+    val value = when {
+        valueText.isEmpty() -> 0.0
+        else -> {
+            valueText = valueText.insert(valueText.length - 2, '.')
+            valueText.toDouble()
+        }
+    }
+
+    val baseCurrencyText = TextFieldValue(
+        text = CurrencyUtils.formatCurrency(value),
+        selection = TextRange(CurrencyUtils.formatCurrency(value).length)
+    )
+    val targetCurrencyText =
+        TextFieldValue(text = CurrencyUtils.formatCurrency(value * currencyRate))
+
+    return Pair(baseCurrencyText, targetCurrencyText)
+}
