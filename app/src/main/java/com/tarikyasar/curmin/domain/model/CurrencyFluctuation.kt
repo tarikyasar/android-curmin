c4db3a45db474fc4ef1ce9d3e71bcc5873484687
Tarik Yasar <m.tarikyasar@gmail.com>
2022-12-03 12:31:03 +0300

    Add not rounded change amount to CurrencyFluctuation class

--- app/src/main/java/com/tarikyasar/curmin/domain/model/CurrencyFluctuation.kt
+++ app/src/main/java/com/tarikyasar/curmin/domain/model/CurrencyFluctuation.kt
@@ -4,7 +4,8 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class CurrencyFluctuation(
-    @SerializedName("change") val change: Double,
+    @SerializedName("change") val changeRounded: Double,
+    @SerializedName("change_pct") val change: Double,
     @SerializedName("end_rate") val endRate: Double,
     @SerializedName("start_rate") val startRate: Double,
 ) : Serializable
